d0c3891339e791a0a49a16a6a0ca47114f0d65c8
John Reiley <rei15006@byui.edu>
2019-06-24 16:34:32 -0600

    Made Prompt class public and set up tests to work with the project

---
+++ test/air_nomads_project2.Tests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace air_nomades_projectSquared.Tests
+{
+    public class PrompterTest
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.Equal(0, 0);
+        }
+    }
+}
